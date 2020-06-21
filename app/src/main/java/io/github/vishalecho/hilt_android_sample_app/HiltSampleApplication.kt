052e811624126fcc55f1c1ca44f0491b1f426656
vishalecho <vkr1994@gmail.com>
2020-06-21 13:37:44 +0530

    hilt project setup completed

---
+++ app/src/main/java/io/github/vishalecho/hilt_android_sample_app/HiltSampleApplication.kt
@@ -0,0 +1,9 @@
+package io.github.vishalecho.hilt_android_sample_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltSampleApplication : Application() {
+
+}
--- app/src/main/java/io/github/vishalecho/hilt_android_sample_app/ui/MainActivity.kt
+++ app/src/main/java/io/github/vishalecho/hilt_android_sample_app/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package io.github.vishalecho.hilt_android_sample_app
+package io.github.vishalecho.hilt_android_sample_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.vishalecho.hilt_android_sample_app.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
